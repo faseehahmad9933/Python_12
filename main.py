9f919eeac5e2118dee6d7c81b1a4b18d4cc2d906
Faseeh Ahmad <faseehkamboh9933>
2025-02-22 16:45:48 +0500

    second commit

---
+++ main.py
@@ -0,0 +1,27 @@
+# import random
+# random_number = random.randint(1, 6)
+# user_input = int(input("Enter a number"))
+
+# while(True):
+
+#     while(random_number == user_input):
+#         print("you guesssed the right Number!") 
+#         break
+#     else:
+#         print("your guess is wrong")
+
+
+# multiplke returns are not aalowed in a function
+# def add(first_num:int, second_num:int) -> int:
+#     return first_num + second_num
+# add(1, 2) # this will return None
+# result:int = add(5,8)
+# print(result)   
+# tuple = (1,2,3,4)
+# print(type(tuple))
+
+
+# default value
+# positional arguments
+# 
+
